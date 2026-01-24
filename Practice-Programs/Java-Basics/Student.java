3584f121e88eccfd385de8a85bb859554e297e44
mayank7720 <mydv986@gmail.com>
2026-01-24 14:01:07 +0530

    [Mayank] adding basic programs

---
+++ Practice-Programs/Java-Basics/Student.java
@@ -0,0 +1,9 @@
+//BAsic Java program
+
+class Student{ // class : Class keyword with className
+public static void main(String args[]){ //Main method JVM internally call
+
+System.out.println("Hello World"); // Printing statement
+
+}
+}// curly braces
